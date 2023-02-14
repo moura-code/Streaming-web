a18a40fe59ad1d0d60e54b3f32e35b07c1cf9dcb
joao calado <jcaladomoura@gmail.com>
2023-02-14 01:10:47 -0300

    Add m3u playlist format support

--- frontend/components/videoPlayer.tsx
+++ frontend/components/videoPlayer.tsx
@@ -1,12 +1,41 @@
+import { useEffect, useRef, useState } from "react";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
 
-
-function VideoPlay() {
+function VideoPlay ()  {
+  const play = {
+    fill: true,
+    fluid: true,
+    autoplay: true,
+    controls: true,
+    preload: "metadata",
+    sources: [
+      {
+        src: "https://v3.szjal.cn/20191101/PZzNpqB1/index.m3u8",
+        type: "application/x-mpegURL"
+      }
+    ]
+  };
+  const videoNode = useRef(null);
+  const [player, setPlayer] = useState(null);
+  useEffect(() => {
+    if (videoNode.current) {
+      const _player = videojs(videoNode.current, play);
+      setPlayer(_player);
+      return () => {
+        if (player !== null) {
+          player.dispose();
+        }
+      };
+    }
+  }, []);
   
   return (
-    <div >
-      pene
+    <div data-vjs-player>
+      <video ref={videoNode} className="video-js"></video>
     </div>
   );
-}
+};
+
 
 export default VideoPlay;
