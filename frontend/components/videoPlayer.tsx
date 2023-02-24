6e725d93be9406be8a72c5a57515219ce9093239
joao calado <jcaladomoura@gmail.com>
2023-02-23 22:29:35 -0300

    auth update

--- frontend/components/videoPlayer.tsx
+++ frontend/components/videoPlayer.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useRef, useState } from "react";
+import { Suspense, useEffect, useRef, useState } from "react";
 import videojs from "video.js";
-import Script from 'next/script'
+import Script from "next/script";
 import "video.js/dist/video-js.css";
+import Hls from 'react-hls';
+import ReactPlayer from "react-player";
 const play = {
   fill: true,
   fluid: true,
@@ -10,18 +12,18 @@ const play = {
   preload: "none",
   sources: [
     {
-      src: "../public/playlist_Joao8095_plus.m3u",
+      src: "http://disfruta.net:80/tvlatvgo152728e/XfnGyhSif8/1059",
       type: "application/x-mpegURL",
     },
   ],
   html5: {
     hls: {
-      withCredentials: true
-    }
-  }
+      withCredentials: true,
+    },
+  },
 };
-function VideoPlay() {
-  
+
+function VideoPlay1() {
   const videoNode = useRef(null);
   const [player, setPlayer] = useState(null);
 
@@ -41,12 +43,14 @@ function VideoPlay() {
   return (
     <div data-vjs-player>
       <video ref={videoNode} className="video-js "></video>
-      <Script src="videojs-contrib-hls.min.js"onLoad={() => {
-          console.log('Script has loaded')
-        }} async/>
     </div>
-    
   );
 }
-
+function VideoPlay() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Hls url="http://disfruta.net:80/tvlatvgo152728e/XfnGyhSif8/1059" />
+    </Suspense>
+  );
+}
 export default VideoPlay;
