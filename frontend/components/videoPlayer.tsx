2d0411d0a42d23f5a3427abd9427845296397bcd
Pedro Calado Moura <pcaladomoura@gmail.com>
2023-04-09 17:08:19 -0300

    update

--- frontend/components/videoPlayer.tsx
+++ frontend/components/videoPlayer.tsx
@@ -1,36 +1,27 @@
 import { Suspense, useEffect, useRef, useState } from "react";
 import videojs from "video.js";
 import "video.js/dist/video-js.css";
-import "videojs-contrib-hls"
+
 import { playerLog } from "../libs";
 import Script from "next/script";
-
+import ReactPlayer from  'react-player'
 const play = {
   fill: true,
   fluid: true,
   autoplay: true,
   controls: true,
-  preload: "metadata",
+  preload: "none",
   sources: [
     {
-      src: "http://xyz.lattv.com.co:8080/Smartvpremium/Smartvpremium/150805",
+      src : "162.212.152.104:25461/RCNWeb-1/Live/36",
       type: "application/x-mpegURL",
     },
   ],
-  html5: {
-    hls: {
-      enableLowInitialPlaylist: true,
-      smoothQualityChange: true,
-      overrideNative: true,
-      useNetworkInformationApi: true,
-      experimentalBufferBasedABR: true
-    }
-  },
   liveui: true
   
 };
 
-function VideoPlay() {
+function VideoPlay1() {
 
   const videoNode = useRef(null);
   const [player, setPlayer] = useState(null);
@@ -55,24 +46,29 @@ function VideoPlay() {
   return (
     <div data-vjs-player>
       <video ref={videoNode} className="video-js"></video>
-      <link
-        href="https://vjs.zencdn.net/7.15.4/video-js.min.css"
-        rel="stylesheet"
-        integrity="sha384-qtMeiqNYAEkWJHm4yfDJi9XsCO4YFjRUcGtRZP/IS/AMKpZlLqG3qll60dD9okzt"
-        crossOrigin="anonymous"
-      />
-      <Script
-        src={`https://cdn.jsdelivr.net/npm/hls.js@latest`}
-        integrity="sha384-nfDWkwLZm+MQCYH9V7xmx/aKvJgigB49+oSiio5bKavhAWZgPO7IaMvgO9N+ulqO"
-        crossOrigin="anonymous"
-        
-      />
-      <Script
-        src={`https://cdn.jsdelivr.net/npm/videojs-contrib-hls@latest/dist/videojs-contrib-hls.min.js`}
-        integrity="sha384-hVhJZGzv7R+L02c9B7VWTxgj2NyPAyfl84tuwz5It5RYn8pGVQuhZKjSy/UdX0NR"
-        crossOrigin="anonymous"
-      />
     </div>
     )
+}
+function VideoPlay() {
+  const videoNode = useRef(null);
+  const [player, setPlayer] = useState(null);
+  useEffect(() => {
+    if (videoNode.current) {
+      const _player = videojs(videoNode.current, play);
+      setPlayer(_player);
+      return () => {
+        if (player !== null) {
+          player.dispose();
+        }
+      };
+    }
+  }, []);
+
+  return (
+    <div data-vjs-player>
+      <video ref={videoNode} className="video-js"></video>
+    </div>
+  );
+
 }
 export default VideoPlay;
